e8940d1630393f47625253aeb84f4b7f6ccddbb3
bebob94 <bebo.macis@gmail.com>
2023-02-11 11:23:38 +0100

    add

--- src/Components/MyCards.jsx
+++ src/Components/MyCards.jsx
@@ -2,12 +2,14 @@
 import { Component } from "react";
 import Loading from "./Loading";
 import Error from "./Error";
-
+import { Modal, Button } from "react-bootstrap";
 class MyCards extends Component {
   state = {
     isLoading: true,
     isError: false,
     query: [],
+    modalOpen: false,
+    selectedMovie: {},
   };
 
   componentDidMount = async () => {
@@ -34,13 +36,25 @@ class MyCards extends Component {
     }
   };
 
+  openModal = async (movie) => {
+    this.setState({ modalOpen: true, selectedMovie: movie });
+    // console.log(this.state.selectedMovie);
+    console.log(movie);
+  };
+
+  closeModal = async () => {
+    this.setState({ modalOpen: false });
+    console.log(this.state.modalOpen);
+  };
+
   render() {
     return (
       <>
         {this.state.isLoading && <Loading />}
         {this.state.isError && <Error />}
-        {this.state.query.map((movie) => (
+        {this.state.query.slice(0, 8).map((movie) => (
           <div
+            onClick={() => this.openModal(movie)}
             key={movie.imdbID}
             style={{
               display: "inline-block",
@@ -51,10 +65,27 @@ class MyCards extends Component {
             <img
               className="m-2 mb-4 "
               src={movie.Poster}
-              style={{ height: "200px", width: "150px" }}
+              style={{ height: "180px", width: "120px" }}
             ></img>
-            <p className="tronca">{movie.Title}</p>
-            <p>{movie.Year}</p>
+
+            <Modal show={this.state.modalOpen} onHide={this.closeModal}>
+              <Modal.Header closeButton>
+                <Modal.Title>
+                  Titolo: {this.state.selectedMovie.Title}
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{`Genere: ${this.state.selectedMovie.Genre}`}</p>
+                <p>{`Anno: ${this.state.selectedMovie.Year}`}</p>
+                <p>{`Regista: ${this.state.selectedMovie.Director}`}</p>
+                <p>{`Attori: ${this.state.selectedMovie.Actors}`}</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={this.closeModal}>
+                  Chiudi
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </div>
         ))}
       </>
